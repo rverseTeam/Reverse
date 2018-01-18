18e78001e117aa6ae723a8eb9345ae581c194164
Repflez <Repflez@users.noreply.github.com>
2018-01-18 12:00:30 -0700

    Add command system

---
+++ src/Console/Command/DatabaseRollbackCommand.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Holds the migration rollback command controller.
+ * @package Miiverse
+ */
+
+namespace Miiverse\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Filesystem\Filesystem;
+use Miiverse\DB;
+
+/**
+ * Rolls back the last database migration action.
+ * @package Miiverse
+ * @author Repflez
+ */
+class DatabaseRollbackCommand extends Command
+{
+	/**
+	 * Set up the command metadata.
+	 */
+	protected function configure() : void {
+		$this->setName('db:rollback')->setDescription('Rollback the last database migration.')->setHelp('This command rollbacks the last database migration. Useful if the migration doesn\'t work.')
+		;
+	}
+
+	/**
+	 * Does the rolling back.
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$repository = DB::getMigrationRepository();
+		$migrator = new Migrator($repository, $repository->getConnectionResolver(), new Filesystem);
+
+		$migrator->rollback();
+
+		foreach ($migrator->getNotes() as $note) {
+			$output->writeln(strip_tags($note));
+		}
+	}
+}
---
+++ src/Console/Command/DatabaseStatusCommand.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Holds the migration status command controller.
+ * @package Miiverse
+ */
+
+namespace Miiverse\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Filesystem\Filesystem;
+use Miiverse\DB;
+
+/**
+ * Returns the status of the database migrations.
+ * @package Miiverse
+ * @author TestVerse
+ */
+class DatabaseStatusCommand extends Command
+{
+	/**
+	 * The database migrations directory.
+	 */
+	private const MIGRATIONS = "database/";
+
+	/**
+	 * Set up the command metadata.
+	 */
+	protected function configure() : void {
+		$this->setName('db:status')->setDescription('Show the status of each migration.')->setHelp('This command shows the status of each migration.')
+		;
+	}
+
+	/**
+	 * Fulfills the purpose of what is described above this class.
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$repository = DB::getMigrationRepository();
+		$migrator = new Migrator($repository, $repository->getConnectionResolver(), new Filesystem);
+
+		if (!$migrator->repositoryExists()) {
+			$output->writeln("No migrations found!");
+			return 0;
+		}
+
+		$ran = $repository->getRan();
+
+		$migrations = new Table($output);
+
+		$migrations->setHeaders([
+			'Ran?',
+			'Migration',
+		]);
+
+		foreach ($migrator->getMigrationFiles(path(self::MIGRATIONS)) as $migration) {
+			$migrations->addRow([in_array($migration, $ran) ? 'Y' : 'N', $migration]);
+		}
+
+		$migrations->render();
+	}
+}
