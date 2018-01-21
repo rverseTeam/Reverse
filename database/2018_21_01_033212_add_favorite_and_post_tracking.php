686b6ac3a2ec9128bf70ffa4278d7e29d52e6f99
Repflez <Repflez@users.noreply.github.com>
2018-01-21 14:33:39 -0700

    rip

--- database/2018_21_01_033212_add_favorite_and_post_tracking.php
+++ database/2018_21_01_033212_add_favorite_and_post_tracking.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Miiverse\DB;
 
-class AddUserIdInPostsTable extends Migration
+class AddFavoriteAndPostTracking extends Migration
 {
 	/**
 	 * Run the migrations.
