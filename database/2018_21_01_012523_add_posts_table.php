cb0a10fceda73be8aa5487273bfdf0379260844f
Repflez <Repflez@users.noreply.github.com>
2018-01-21 01:25:50 -0700

    Add posts table

---
+++ database/2018_21_01_012523_add_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Miiverse\DB;
+
+class AddPostsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 * @return void
+	 */
+	public function up()
+	{
+		$schema = DB::getSchemaBuilder();
+
+		$schema->create('posts', function (Blueprint $table) {
+			$table->increments('id');
+
+			$table->timestampTz('created')->useCurrent();
+
+			$table->timestampTz('edited')->useCurrent();
+
+			$table->softDeletesTz();
+
+			$table->integer('community')->unsigned()->default(0);
+
+			$table->text('content');
+
+			$table->string('image', 250)->nullable();
+
+			$table->tinyInteger('feeling')->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 * @return void
+	 */
+	public function down()
+	{
+		$schema = DB::getSchemaBuilder();
+
+		$schema->drop('posts');
+	}
+}
