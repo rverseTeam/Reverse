0d78453c10eec349ef57f28e028a7e542a375b21
Repflez <Repflez@users.noreply.github.com>
2018-01-20 22:03:46 -0700

    I love typos...

--- database/2018_20_01_215309_fix_community_perm_enum.php
+++ database/2018_20_01_215309_fix_community_perm_enum.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Miiverse\DB;
 
-class FixCocmmunityPermEnum extends Migration
+class FixCommunityPermEnum extends Migration
 {
 	/**
 	 * Run the migrations.
