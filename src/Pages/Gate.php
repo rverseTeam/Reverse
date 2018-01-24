d094689413264199a4401096077c1824c6908dea
Repflez <Repflez@users.noreply.github.com>
2018-01-24 14:56:10 -0700

    Try to make it use te already made reference in Gate

--- src/Pages/Gate.php
+++ src/Pages/Gate.php
@@ -43,7 +43,7 @@ class Gate extends Page
 		]);
 
 		// Get all Mii images and save them to the mapping table
-		$id = Mii::get($_POST['welcome_nnid']);
+		$id = Mii::get($user->nnid);
 
 		$miis_temp = Mii::getMiiImages($id);
 
