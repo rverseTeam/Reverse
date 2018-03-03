db8d01f62273791ae1c1305598d15a2575c13c88
Steph Lockhomes <antherynx.studio@mail.com>
2018-03-03 18:01:15 -0500

    ok debug over

--- src/Pages/Title/Community.php
+++ src/Pages/Title/Community.php
@@ -122,8 +122,6 @@ class Community extends Page
 
             $feeling = ['normal', 'happy', 'like', 'surprised', 'frustrated', 'puzzled'];
 
-            var_dump($discussions);
-
             return view('titles/view_redesign', compact('meta', 'topicCategories', 'drawings', 'discussions', 'feeling'));
         } else {
             $posts_pre = DB::table('posts')
