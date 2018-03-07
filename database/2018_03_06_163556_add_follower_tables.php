b2650f8b7a5f9abf3e4b5e6f498ba4ffcaceb95d
Steph Lockhomes <antherynx.studio@mail.com>
2018-03-06 19:38:01 -0500

    Quick hotfix

--- database/2018_03_06_163556_add_follower_tables.php
+++ database/2018_03_06_163556_add_follower_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Miiverse\DB;
 
-class FixRedesignDefaultValues extends Migration
+class AddFollowerTables extends Migration
 {
     /**
      * Run the migrations.
