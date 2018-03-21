63105347a757e6ad68840ae6fea4bd80480c823f
Steph Lockhomes <antherynx.studio@mail.com>
2018-03-20 23:49:48 -0400

    WTF NINTENDO?

--- src/Pages/Index.php
+++ src/Pages/Index.php
@@ -60,9 +60,9 @@ class Index extends Page
     public function latestActivityFeed() : string
     {
         return '
-    <div class="data-latest-following-relation-profile-post-placeholder"><div class="window">
+    <data-latest-following-relation-profile-post-placeholder><div class="window">
       <p>There are no posts to display.</p>
-    </div></div>
+    </div></data-latest-following-relation-profile-post-placeholder>
         ';
     }
 }
